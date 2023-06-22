5c40b8ef4b1fd9e4f7526dc890f031391da2ecd4
Serge Ballesta <serge.ballesta@sfr.fr>
2023-06-22 23:36:33 +0200

    Add first views.

---
+++ iso9001/core/urls.py
@@ -0,0 +1,10 @@
+"""URL configuration for core app."""
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('process', views.ProcessesList.as_view(), name="processes"),
+    path('axis', views.AxesList.as_view(), name="axes"),
+]
--- iso9001/core/views.py
+++ iso9001/core/views.py
@@ -1,3 +1,42 @@
+"""Views for the core app"""
+from typing import Any, Dict
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from django.utils.translation import gettext as _
+from .models import Process, PolicyAxis
+
 
 # Create your views here.
+def home(request) -> HttpResponse:
+    """The top level index view"""
+    return render(request, 'core/index.html')
+
+
+class ProcessesList(generic.ListView):
+    """List processes.
+
+    By default, only applicable processes are available.
+    """
+    queryset = Process.objects.filter(status=Process.Status.APPLICABLE)
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        """Add a title to context_data"""
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('Processes')
+        return context
+
+
+class AxesList(generic.ListView):
+    """List quality axes.
+
+    By default, only applicable axes are available.
+    """
+    model = PolicyAxis
+    template_name = 'core/process_list.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        """Add a title to context_data"""
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('Policy axes')
+        return context
