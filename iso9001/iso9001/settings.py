8bf514e98c7d180574ac2c5563636b765c989bfb
Serge Ballesta <serge.ballesta@sfr.fr>
2023-06-25 09:09:00 +0200

    Allow local configuration of the mailer backend.
    Also use / for the default target page after a successful login.

--- iso9001/iso9001/settings.py
+++ iso9001/iso9001/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+
+# EMAIL config
+EMAIL_BACKEND = config('EMAIL_BACKEND',
+                       default='django.core.mail.backends.smtp.EmailBackend')
