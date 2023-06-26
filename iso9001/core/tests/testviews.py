899d7d2fd4c2aad6889f144e0eb2ee098e1a0df8
Serge Ballesta <serge.ballesta@sfr.fr>
2023-06-26 19:48:11 +0200

    Add tests for the views.

---
+++ iso9001/core/tests/testviews.py
@@ -0,0 +1,101 @@
+"""Tests for the admin site"""
+
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+from django.urls import reverse
+from core.models import Process, StatusModel, PolicyAxis
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    BeautifulSoup = None
+
+User = get_user_model()
+
+
+class TestViews(TestCase):
+    """Test for core app views"""
+    @classmethod
+    def setUpTestData(cls) -> None:
+        """Install 2 processes, 1 admin user and 1 normal user"""
+        cls.admin = User.objects.create_superuser(username='admin')
+        cls.user1 = User.objects.create_user(username='user1')
+        cls.p1 = Process.objects.create(name='P1', desc='Prod 1',
+                                        status=StatusModel.Status.APPLICABLE)
+        cls.p2 = Process.objects.create(name='P2', desc='Prod 2')
+
+    def test_base_not_connected(self) -> None:
+        """Ensure menu contains a connection entry"""
+        client = Client()
+        resp = client.get(reverse('home'))
+        self.assertEqual(200, resp.status_code)
+        soup = BeautifulSoup(resp.content)
+        menu = soup.find(attrs={'id': 'menu'})
+        url = reverse('login')
+        atag = menu.css.select(f'a[href^="{url}"]')
+        self.assertTrue('Connect' in atag[0].text)
+
+    def test_base_connected(self) -> None:
+        """Ensure menu contains the user name and no connection entry"""
+        client = Client()
+        client.force_login(self.user1)
+        resp = client.get(reverse('home'))
+        self.assertEqual(200, resp.status_code)
+        soup = BeautifulSoup(resp.content)
+        menu = soup.find(attrs={'id': 'menu'})
+        url = reverse('login')
+        atag = menu.css.select(f'a[href^="{url}"]')
+        self.assertEqual(0, len(atag))
+        for tag in menu.find_all():
+            if 'user1' in tag.text:
+                return
+        self.fail('username not found in menu')
+
+    def test_process_list(self) -> None:
+        """Ensure that we get the applicable process"""
+        client = Client()
+        perm = Permission.objects.get(
+            content_type__app_label='core',
+            content_type__model='process',
+            codename='view_process',
+        )
+        self.user1.user_permissions.add(perm)
+        self.user1.save()
+        client.force_login(self.user1)
+        resp = client.get(reverse('processes'))
+        self.assertEqual(200, resp.status_code)
+        soup = BeautifulSoup(resp.content)
+        names = soup.findAll('th', attrs={'scope': 'row'})
+        self.assertEqual(1, len(names))
+        self.assertEqual('P1', names[0].text)
+
+    def test_process_no_perm(self) -> None:
+        """Ensure that view_process permission is required"""
+        client = Client()
+        client.force_login(self.user1)
+        resp = client.get(reverse('processes'))
+        self.assertEqual(403, resp.status_code)
+
+    def test_axes_list(self) -> None:
+        """Ensure that we can list applicable axes"""
+        PolicyAxis.objects.create(name='A1', desc='Axe 1',
+                                       status=StatusModel.Status.APPLICABLE)
+        PolicyAxis.objects.create(name='A2', desc='Axe 2')
+        PolicyAxis.objects.create(name='A3', desc='Axe 3',
+                                       status=StatusModel.Status.APPLICABLE)
+        client = Client()
+        perm = Permission.objects.get(
+            content_type__app_label='core',
+            content_type__model='policyaxis',
+            codename='view_policyaxis',
+        )
+        self.user1.user_permissions.add(perm)
+        self.user1.save()
+        client.force_login(self.user1)
+        resp = client.get(reverse('axes'))
+        self.assertEqual(200, resp.status_code)
+        soup = BeautifulSoup(resp.content)
+        names = soup.findAll('th', attrs={'scope': 'row'})
+        self.assertEqual(2, len(names))
+        self.assertEqual('A1', names[0].text)
+        self.assertEqual('A3', names[1].text)
--- iso9001/core/views.py
+++ iso9001/core/views.py
@@ -3,6 +3,7 @@ from typing import Any, Dict
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.utils.translation import gettext as _
 from .models import Process, PolicyAxis
 
@@ -13,11 +14,12 @@ def home(request) -> HttpResponse:
     return render(request, 'core/index.html')
 
 
-class ProcessesList(generic.ListView):
+class ProcessesList(PermissionRequiredMixin, generic.ListView):
     """List processes.
 
     By default, only applicable processes are available.
     """
+    permission_required = "core.view_process"
     queryset = Process.objects.filter(status=Process.Status.APPLICABLE)
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
@@ -27,13 +29,14 @@ class ProcessesList(generic.ListView):
         return context
 
 
-class AxesList(generic.ListView):
+class AxesList(PermissionRequiredMixin, generic.ListView):
     """List quality axes.
 
     By default, only applicable axes are available.
     """
-    model = PolicyAxis
+    queryset = PolicyAxis.objects.filter(status=Process.Status.APPLICABLE)
     template_name = 'core/process_list.html'
+    permission_required = 'core.view_policyaxis'
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         """Add a title to context_data"""
