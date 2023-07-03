45cc11d1556bf5ae696fe6fc583b51ff616c65ce
Serge Ballesta <serge.ballesta@sfr.fr>
2023-07-03 14:15:47 +0200

    Fix a mistake in ALLOWED_HOSTS config.

--- iso9001/iso9001/settings.py
+++ iso9001/iso9001/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config('SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default=['*'])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='*')
 
 
 # Application definition
